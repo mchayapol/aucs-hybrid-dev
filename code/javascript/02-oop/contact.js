49d53d6cafabaf8539c417f8b4b942e73da40102
Chayapol Moemeng <mchayapol@gmail.com>
2015-08-31 10:08:53 +0700

    Update Contact App examples

--- code/javascript/02-oop/contact.js
+++ code/javascript/02-oop/contact.js
@@ -1,13 +1,19 @@
-function Contact(fname,lname,phone) {
+function Contact(fname, lname, phone) {
     this.firstname = fname;
     this.lastname = lname;
     this.phone = phone;
+    this.email = '';    // not in param list, not required
+    this.picture = '';
     this.fullname = function() {
         return this.firstname+' '+this.lastname;
+    }
+
+    this.sayHello = function () {
+        console.log( "Hello"  );
     }
 };
 
 
-Contact.sayHi = function() {
+Contact.prototype.sayHi = function() {
     console.log("Hi!");
 }
