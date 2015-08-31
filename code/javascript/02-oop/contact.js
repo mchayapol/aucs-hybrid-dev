8f93b368e3e9c137ad76ffc76055b6260c35194a
Patipan Premrat <talkwithbas02@hotmail.com>
2015-08-31 23:53:47 +0700

    commit JAVASCRIPT OOP

--- code/javascript/02-oop/contact.js
+++ code/javascript/02-oop/contact.js
@@ -1,24 +1,15 @@
-function Contact(fname, lname, phone) {
+function Contact(fname,lname,phone) {
     this.firstname = fname;
     this.lastname = lname;
     this.phone = phone;
-<<<<<<< HEAD
     this.email = 'sample@email.com';
     this.picture = 'inventech.png';
-=======
-    this.email = '';    // not in param list, not required
-    this.picture = '';
->>>>>>> 7ba196852d87405deff024d7b23f0d39ad8ad97f
     this.fullname = function() {
         return this.firstname + ' ' +this.lastname;
     }
 
     this.sayHello = function(){
     	console.log("Hello!");
-    }
-
-    this.sayHello = function () {
-        console.log( "Hello"  );
     }
 };
 
